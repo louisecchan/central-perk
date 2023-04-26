20e1a086170c6e662ade4d3578dd98d8c0fc734b
Louise <louise.cch@gmail.com>
2023-04-26 17:01:29 +0100

    Add mobile responsiveness

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -10,11 +10,13 @@ function HomePage() {
         <div className="bg-light p-5 m-5 rounded-3">
           <h1>Welcome to Central Perk POS</h1>
           <br />
-          <p>
+          <p className="pos-desc">
             The bespoke POS system built for coffee shops of all shapes and
             sizes.
           </p>
-          <p>If you have an issue, call 020 7101 7661 anytime.</p>
+          <p className="pos-desc">
+            If you have an issue, call 020 7101 7661 anytime.
+          </p>
           <br />
           <Link to="/pos" className="btn">
             Click here to start
