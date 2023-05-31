d0661e30d802fe21d9db64cf7f16d8ebfc68158c
Louise <louise.cch@gmail.com>
2023-05-31 12:01:18 +0100

    Joi leng jai siu siu

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -10,13 +10,14 @@ function HomePage() {
         <div className="bg-light p-5 m-5 rounded-3">
           <h1>Welcome to Central Perk POS</h1>
           <br />
-          <p className="pos-desc">
+          <p className="pos-desc-1">
             The bespoke POS system built for coffee shops of all shapes and
-            sizes. <br /> If you have an issue, call 020 7101 7661 anytime.
+            sizes.
+            {/* <br /> If you have an issue, call 020 7101 7661 anytime. */}
           </p>
-          {/* <p className="pos-desc">
+          <p className="pos-desc-1 pos-desc-2">
             If you have an issue, call 020 7101 7661 anytime.
-          </p> */}
+          </p>
           <br />
           <br />
           <br />
