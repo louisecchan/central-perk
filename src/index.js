f1864f7286fef3fa38f6154afc308ff50bb3216b
Louise <louise.cch@gmail.com>
2023-06-26 17:14:24 +0300

    Update font

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import "./fonts/Sohne-Buch.otf";
-import "./fonts/Aspira-Bold.otf";
+import "./fonts/Aspira-Medium.otf";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(<App />);
