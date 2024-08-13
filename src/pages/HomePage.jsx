22ab5a95f242d8f01a54cbdf7983c13cd74309d3
louisechan <louise.chan@101medialab.com>
2024-08-13 13:20:02 -0700

    Mobile & tablet

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -10,8 +10,8 @@ function HomePage() {
         <div className="bg-black p-5 m-5 rounded-3">
           <p className="pos-desc-1">
             Built for cafes.{" "}
+            Built for you.
             <span className="contact-text">
-              Built for you.
             </span>
           </p>
           <br />
