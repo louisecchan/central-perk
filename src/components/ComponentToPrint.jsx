cc8f645d2d09fb5462299a33608c7dc6d4a452fe
louisechan <louise.chan@101medialab.com>
2024-08-17 00:33:06 -0700

    Add heading to receipt

--- src/components/ComponentToPrint.jsx
+++ src/components/ComponentToPrint.jsx
@@ -3,34 +3,37 @@ import React from "react";
 export const ComponentToPrint = React.forwardRef((props, ref) => {
   const { cart, totalAmount } = props;
   return (
-    <div ref={ref} className="p-5">
-      <table className="table">
-        <thead>
-          <tr>
-            <td>#</td>
-            <td>Name</td>
-            <td>Price</td>
-            <td>Qty</td>
-            <td>Total</td>
-          </tr>
-        </thead>
-        <tbody>
-          {cart
-            ? cart.map((cartProduct, key) => (
-                <tr key={key}>
-                  <td>{cartProduct.id}</td>
-                  <td>{cartProduct.name}</td>
-                  <td>{cartProduct.price}</td>
-                  <td>{cartProduct.quantity}</td>
-                  <td>{cartProduct.totalAmount}</td>
-                </tr>
-              ))
-            : ""}
-        </tbody>
-      </table>
-      <h6 className="px-2">Total Amount: ${totalAmount}</h6>
-      <br></br>
-      <h6 className="px-2">Thank you!</h6>
-    </div>
+      <div ref={ref} className="p-5">
+          <table className="table">
+              <thead>
+              <tr className="receipt-heading">
+                  <th colspan="5">Central Perk</th>
+              </tr>
+              <tr className="receipt-subheading">
+              <td>#</td>
+                  <td>Name</td>
+                  <td>Price</td>
+                  <td>Qty</td>
+                  <td>Total</td>
+              </tr>
+              </thead>
+              <tbody>
+              {cart
+                  ? cart.map((cartProduct, key) => (
+                      <tr key={key}>
+                          <td>{cartProduct.id}</td>
+                          <td>{cartProduct.name}</td>
+                          <td>{cartProduct.price}</td>
+                          <td>{cartProduct.quantity}</td>
+                          <td>{cartProduct.totalAmount}</td>
+                      </tr>
+                  ))
+                  : ""}
+              </tbody>
+          </table>
+          <h6 className="px-2">Total Amount: ${totalAmount}</h6>
+          <br></br>
+          <h6 className="px-2">Thank you!</h6>
+      </div>
   );
 });
